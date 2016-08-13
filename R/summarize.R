3cbede12eef1096ec375b85565df7d8ed6abe8d3
Colton <mccurdyc@allegheny.edu>
2016-08-12 22:01:48 -0400

    American English

--- R/summarize.R
+++ R/summarize.R
@@ -1,19 +1,19 @@
-#' FUNCTION: summarise_reduced_time
+#' FUNCTION: summarize_reduced_time
 #'
 #' Calculate the total time (in ms) of performing mutation analysis with the given set of mutants.
 #' @export
 
-summarise_reduced_time <- function(d) {
+summarize_reduced_time <- function(d) {
   dt <- d %>% dplyr::group_by(dbms, schema) %>% dplyr::summarise(reduced_time = sum(time))
   return(dt)
 }
 
-#' FUNCTION: summarise_original_time
+#' FUNCTION: summarize_original_time
 #'
 #' Calculate the total time (in ms) of performing mutation analysis with the given set of mutants.
 #' @export
 
-summarise_original_time <- function(d) {
+summarize_original_time <- function(d) {
   dt <- d %>% dplyr::group_by(dbms, schema) %>% dplyr::summarise(original_time = sum(time))
   return(dt)
 }

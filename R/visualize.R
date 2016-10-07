38e2fae8b826d5a5fdf9397c8dc07b0dc3c433b9
Colton McCurdy <mccurdyc@allegheny.edu>
2016-10-07 11:18:27 -0400

    fixed defaults of create_random_sampling_graphs function and removed Cairo dependency

--- R/visualize.R
+++ R/visualize.R
@@ -10,7 +10,7 @@ visualize_random_sampling_mutation_scores <- function(d, trans = FALSE) {
   } else {
     name <- "../graphics/from-data/mutation_score_random_plot.pdf"
   }
-  visualize_save_graphic(name, p, 8, 16)
+  visualize_save_graphic(name, p, 8, 8)
   return(p)
 }
 
@@ -38,19 +38,7 @@ visualize_operator_sampling_mutation_scores <- function(d, trans = FALSE) {
 #' @export
 
 visualize_plot_mutation_score <- function(d, trans = FALSE) {
-  if (trans != FALSE) {
-    p <- ggplot2::ggplot(d, ggplot2::aes(x = schema, y = reduced_mutation_score)) +
-    ggplot2::geom_boxplot() +
-    ggplot2::facet_wrap(~ percentage, labeller = ggplot2::label_both) +
-    ggplot2::scale_y_continuous(limits = c(0, 100)) +
-    ggplot2::stat_summary(fun.y = mean, fill = "white", colour = "black", geom = "point", shape = 24, size = 1, show.legend = FALSE) +
-    ggplot2::theme_bw(base_size = 10) +
-    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, hjust = 1, size = 10)) +
-    ggplot2::theme(axis.text.y = ggplot2::element_text(angle = 45, hjust = 1, size = 10)) +
-    ggplot2::xlab("Schema") +
-    ggplot2::ylab("Mutation Score")
-    return(p)
-  } else {
+  if (trans == TRUE) {
     p <- ggplot2::ggplot(d, ggplot2::aes(x = schema, y = reduced_mutation_score)) +
     ggplot2::geom_boxplot(colour = "#073642", fill = "transparent") +
     ggplot2::facet_wrap(~ percentage, labeller = ggplot2::label_both) +
@@ -72,6 +60,18 @@ visualize_plot_mutation_score <- function(d, trans = FALSE) {
     ggplot2::xlab("Schema") +
     ggplot2::ylab("Mutation Score")
     return(p)
+  } else {
+    p <- ggplot2::ggplot(d, ggplot2::aes(x = schema, y = reduced_mutation_score)) +
+    ggplot2::geom_boxplot() +
+    ggplot2::facet_wrap(~ percentage, labeller = ggplot2::label_both) +
+    ggplot2::scale_y_continuous(limits = c(0, 100)) +
+    ggplot2::stat_summary(fun.y = mean, fill = "white", colour = "black", geom = "point", shape = 24, size = 1, show.legend = FALSE) +
+    ggplot2::theme_bw(base_size = 10) +
+    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, hjust = 1, size = 10)) +
+    ggplot2::theme(axis.text.y = ggplot2::element_text(angle = 45, hjust = 1, size = 10)) +
+    ggplot2::xlab("Schema") +
+    ggplot2::ylab("Mutation Score")
+    return(p)
   }
 }
 
@@ -80,7 +80,6 @@ visualize_plot_mutation_score <- function(d, trans = FALSE) {
 #' Saves the provided graphic to the provided name.
 #' @export
 
-visualize_save_graphic <- function(save_name, save_plot, h, w) {
-  ggplot2::ggsave(save_name, save_plot, height = h, width = w)
-  # ggplot2::ggsave(save_name, save_plot, height = h, width = w, device = cairo_pdf)
+visualize_save_graphic <- function(save_name, save_plot, w, h) {
+  ggplot2::ggsave(save_name, save_plot, width = w, height = h)
 }

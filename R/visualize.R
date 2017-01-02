a915578f95c2d2a359a91ef2b32079f10184a31b
Colton McCurdy <mccurdyc@allegheny.edu>
2017-01-02 15:11:54 -0500

    removed TRANS parameter from graphing functions. Renamed operator selection visualization function to selective random

--- R/visualize.R
+++ R/visualize.R
@@ -3,29 +3,21 @@
 #' Visualize mutation scores for the random sampling reduction technique
 #' @export
 
-visualize_random_sampling_mutation_scores <- function(d, trans = FALSE) {
+visualize_random_sampling_mutation_scores <- function(d) {
   p <- visualize_plot_mutation_score(d)
-  if (trans != FALSE) {
-    name <- "../graphics/from-data/mutation_score_random_plot_trans.pdf"
-  } else {
-    name <- "../graphics/from-data/mutation_score_random_plot.pdf"
-  }
+  name <- "../graphics/from-data/mutation_score_random_plot.pdf"
   visualize_save_graphic(name, p, 8, 8)
   return(p)
 }
 
-#' FUNCTION: visualize_operator_sampling_mutation_scores
+#' FUNCTION: visualize_selective_random_mutation_scores
 #'
-#' Visualize mutation scores for the operator sampling reduction technique
+#' Visualize mutation scores for the selective random reduction technique
 #' @export
 
-visualize_operator_sampling_mutation_scores <- function(d, trans = FALSE) {
+visualize_selective_random_mutation_scores <- function(d) {
   p <- visualize_plot_mutation_score(d)
-  if (trans != FALSE) {
-    name <- "../graphics/from-data/mutation_score_operator_plot_trans.pdf"
-  } else {
-    name <- "../graphics/from-data/mutation_score_operator_plot.pdf"
-  }
+  name <- "../graphics/from-data/mutation_score_operator_plot.pdf"
   visualize_save_graphic(name, p, 8, 8)
   return(p)
 }
@@ -37,42 +29,18 @@ visualize_operator_sampling_mutation_scores <- function(d, trans = FALSE) {
 #'
 #' @export
 
-visualize_plot_mutation_score <- function(d, trans = FALSE) {
-  if (trans == TRUE) {
-    p <- ggplot2::ggplot(d, ggplot2::aes(x = schema, y = reduced_mutation_score)) +
-    ggplot2::geom_boxplot(colour = "#073642", fill = "transparent") +
-    ggplot2::facet_wrap(~ percentage, labeller = ggplot2::label_both) +
-    ggplot2::scale_y_continuous(limits = c(0, 100)) +
-    ggplot2::stat_summary(fun.y = mean, fill = "white", colour = "black", geom = "point", shape = 24, size = 1, show.legend = FALSE) +
-    ggplot2::theme_bw(base_size = 20) +
-    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, hjust = 1, size = 20)) +
-    ggplot2::theme(axis.text.y = ggplot2::element_text(angle = 45, hjust = 1, size = 20)) +
-    ggplot2::theme(panel.background = element_rect(fill = "transparent",colour = NA)) +
-    ggplot2::theme(plot.background = element_rect(fill = "transparent",colour = NA)) +
-    ggplot2::theme(panel.grid.major = element_blank()) +
-    ggplot2::theme(panel.grid.minor = element_blank()) +
-    ggplot2::theme(axis.title = element_text(colour = "#073642")) +
-    ggplot2::theme(axis.text = element_text(colour = "#073642", size = 30)) +
-    ggplot2::theme(axis.ticks = element_line(colour = "#073642")) +
-    ggplot2::theme(panel.border = element_rect(colour = "#073642", size = 4)) +
-    ggplot2::theme(strip.background = element_rect(fill="#073642")) +
-    ggplot2::theme(strip.text = element_text(colour = "white")) +
-    ggplot2::xlab("Schema") +
-    ggplot2::ylab("Mutation Score")
-    return(p)
-  } else {
-    p <- ggplot2::ggplot(d, ggplot2::aes(x = schema, y = reduced_mutation_score)) +
-    ggplot2::geom_boxplot() +
-    ggplot2::facet_wrap(~ percentage, labeller = ggplot2::label_both) +
-    ggplot2::scale_y_continuous(limits = c(0, 100)) +
-    ggplot2::stat_summary(fun.y = mean, fill = "white", colour = "black", geom = "point", shape = 24, size = 1, show.legend = FALSE) +
-    ggplot2::theme_bw(base_size = 10) +
-    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, hjust = 1, size = 10)) +
-    ggplot2::theme(axis.text.y = ggplot2::element_text(angle = 45, hjust = 1, size = 10)) +
-    ggplot2::xlab("Schema") +
-    ggplot2::ylab("Mutation Score")
-    return(p)
-  }
+visualize_plot_mutation_score <- function(d) {
+  p <- ggplot2::ggplot(d, ggplot2::aes(x = schema, y = reduced_mutation_score)) +
+  ggplot2::geom_boxplot() +
+  ggplot2::facet_wrap(~ percentage, labeller = ggplot2::label_both) +
+  ggplot2::scale_y_continuous(limits = c(0, 100)) +
+  ggplot2::stat_summary(fun.y = mean, fill = "white", colour = "black", geom = "point", shape = 24, size = 1, show.legend = FALSE) +
+  ggplot2::theme_bw(base_size = 10) +
+  ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, hjust = 1, size = 10)) +
+  ggplot2::theme(axis.text.y = ggplot2::element_text(angle = 45, hjust = 1, size = 10)) +
+  ggplot2::xlab("Schema") +
+  ggplot2::ylab("Mutation Score")
+  return(p)
 }
 
 #' FUNCTION: visualize_save_graphic

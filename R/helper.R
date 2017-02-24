57372b189c730f4c1b0ab2812539b81bc61d69c8
Colton McCurdy <mccurdyc@allegheny.edu>
2017-02-24 13:38:29 -0500

    removing some debugging code

--- R/helper.R
+++ R/helper.R
@@ -46,72 +46,72 @@ helper_incremental <- function(d, partition_size=1) {
     calculate_best_fit() %>%
     collect_schema_data()
 
-    for(j in 1:30) {
+  for(j in 1:30) {
+    dk <- data.frame() # hold the keep data
+    df <- data.frame() # hold the best_fit data
+
+    outside_step <- 1
+    g <- d
+
+    print(paste("TRIAL: ", j))
+    start_position <- d %>% dplyr::count() %>% select_random_start_position()
+    print(paste("START POSITION: ", start_position))
+
+    g <- g %>% helper_bitflip_keep(start_position, partition_size) %>%
+      transform_add_position(start_position) %>%
+      as.data.frame()
+
+    while (TRUE) {
+
+      fst <- TRUE
       dk <- data.frame() # hold the keep data
       df <- data.frame() # hold the best_fit data
 
-      outside_step <- 1
-      g <- d
-
-      print(paste("TRIAL: ", j))
-      start_position <- d %>% dplyr::count() %>% select_random_start_position()
-      print(paste("START POSITION: ", start_position))
-
-      g <- g %>% helper_bitflip_keep(start_position, partition_size) %>%
-        transform_add_position(start_position) %>%
-        as.data.frame()
+      position <- start_position + partition_size
+      print(paste("OUTSIDE STEP: ", outside_step))
 
       while (TRUE) {
 
-        fst <- TRUE
-        dk <- data.frame() # hold the keep data
-        df <- data.frame() # hold the best_fit data
-
-        position <- start_position + partition_size
-        print(paste("OUTSIDE STEP: ", outside_step))
-
-        while (TRUE) {
-
-          if (position == start_position && outside_step == 1) {
-            break
-          }
-
-          k <- g %>% helper_bitflip_keep(position, partition_size) %>%
-            transform_add_position(position) %>%
-            as.data.frame()
-          r <- k %>% collect_keep_data() # only data that was 'kept'
-          da <- evaluate_reduction_technique(d, r) %>%
-            transform_fitness(0.5, 0.5) %>%
-            transform_add_position(position) %>%
-            as.data.frame()
-
-          if (position == start_position && fst != TRUE) {
-            break
-          } else if ((position + partition_size) > nrow(g)) {
-            position <- (position + partition_size) - nrow(d)
-          } else {
-            position <- position + partition_size
-          }
-
-          fst <- FALSE
-          dk <- rbind(dk, k) # keep data
-          df <- rbind(df, da) # best_fit data
-        }
-
-        temp_best_fit <- current_best_fit %>% as.data.frame()
-        b <- df %>% calculate_best_fit() %>% collect_best_fit_data()
-        current_best_fit <- b[!duplicated(b$schema), ] # if ties, only keep one per schema
-        g <- collect_best_keep_data(current_best_fit, dk)
-        outside_step <- outside_step + 1
-
-        # we stop if it is equal because then we are no longer climbing, we have plateaued
-        if ((current_best_fit$best_fit <= temp_best_fit$best_fit)) {
-          a <- temp_best_fit %>% transform_add_start_position(start_position) %>% transform_add_trial(j)
-          dd <- rbind(dd, a)
+        if (position == start_position && outside_step == 1) {
           break
         }
+
+        k <- g %>% helper_bitflip_keep(position, partition_size) %>%
+          transform_add_position(position) %>%
+          as.data.frame()
+        r <- k %>% collect_keep_data() # only data that was 'kept'
+        da <- evaluate_reduction_technique(d, r) %>%
+          transform_fitness(0.5, 0.5) %>%
+          transform_add_position(position) %>%
+          as.data.frame()
+
+        if (position == start_position && fst != TRUE) {
+          break
+        } else if ((position + partition_size) > nrow(g)) {
+          position <- (position + partition_size) - nrow(d)
+        } else {
+          position <- position + partition_size
+        }
+
+        fst <- FALSE
+        dk <- rbind(dk, k) # keep data
+        df <- rbind(df, da) # best_fit data
+      }
+
+      temp_best_fit <- current_best_fit %>% as.data.frame()
+      b <- df %>% calculate_best_fit() %>% collect_best_fit_data()
+      current_best_fit <- b[!duplicated(b$schema), ] # if ties, only keep one per schema
+      g <- collect_best_keep_data(current_best_fit, dk)
+      outside_step <- outside_step + 1
+
+      # we stop if it is equal because then we are no longer climbing, we have plateaued
+      if ((current_best_fit$best_fit <= temp_best_fit$best_fit)) {
+        a <- temp_best_fit %>% transform_add_start_position(start_position) %>% transform_add_trial(j)
+        dd <- rbind(dd, a)
+        break
       }
     }
+  }
   # return(k) # the final reduced keep data telling you which mutants to keep and ignore
   return(dd) # just the actual best_fit values and their respective step for each schema
 }
@@ -123,52 +123,59 @@ helper_incremental <- function(d, partition_size=1) {
 #' @export
 
 helper_incremental_across_schemas <- function(d, s) {
+  dd <- data.frame()
 
   dt <- d %>% transform_add_start_and_step(s) %>% transform_mutant_count() %>% as.data.frame()
   g <- dt # initialize g to original set of mutants
   previous_corr <- 0
   dh <- data.frame()
 
-  st <- 1
-  while (TRUE) {
-    s <- 1
-    dk <- data.frame()
-    df <- data.frame()
-    frst <- TRUE
+  # for (j in 1:30) {
+    # dk <- data.frame()
+    # df <- data.frame()
+    st <- 1
+    # print(paste("TRIAL: ", j))
     while (TRUE) {
-      k <- g %>% helper_flip() %>% transform_add_step(s)
-      k %>% dplyr::glimpse()
-      dk <- rbind(dk, k)
-      r <- k %>% collect_keep_data()
-      current_corr <- evaluate_reduction_technique_across(d, r, s) %>%
-        transform_add_correlation()
-      df <- rbind(df, current_corr)
+      s <- 1
+      dk <- data.frame()
+      df <- data.frame()
+      frst <- TRUE
+      while (TRUE) {
+        k <- g %>% helper_flip() %>% transform_add_step(s)
+        k %>% dplyr::glimpse()
+        dk <- rbind(dk, k)
+        r <- k %>% collect_keep_data()
+        current_corr <- evaluate_reduction_technique_across(d, r, s) %>%
+          transform_add_correlation()
+        df <- rbind(df, current_corr)
 
-      if ((g$position + g$step_size) > g$mutant_count)  {
-        p <- (g$position + g$step_size) - g$mutant_count
-      } else {
-        p <- g$position + g$step_size
+        if ((g$position + g$step_size) > g$mutant_count)  {
+          p <- (g$position + g$step_size) - g$mutant_count
+        } else {
+          p <- g$position + g$step_size
+        }
+        g <- g %>% transform_update_position(p)
+        if (g$position == g$start_position && frst == FALSE) {
+          break
+        }
+        frst <- FALSE
+        s <- s + 1
       }
-      g <- g %>% transform_update_position(p)
-      if (g$position == g$start_position && frst == FALSE) {
+      previous_corr <- current_corr
+      b <- df %>% calculate_highest_correlation() %>% collect_highest_correlation_data()
+      highest_correlation_data <- b[!duplicated(b$schema), ] # if ties, only keep one per schema
+      highest_correlation_data %>% dplyr::glimpse()
+      g <- collect_best_step_data(highest_correlation_data, dk)
+      g %>% dplyr::glimpse()
+      print(paste("STEP OUTSIDE", st))
+      st <- st + 1
+      if (current_corr < previous_corr) {
+        # if (current_corr < previous_corr || st == 3) {
         break
       }
-      frst <- FALSE
-      s <- s + 1
-    }
-    if (current_corr < previous_corr || st == 3) {
-      break
-    }
-    previous_corr <- current_corr
-    b <- df %>% calculate_highest_correlation() %>% collect_highest_correlation_data()
-    highest_correlation_data <- b[!duplicated(b$schema), ] # if ties, only keep one per schema
-    highest_correlation_data %>% dplyr::glimpse()
-    g <- collect_best_step_data(highest_correlation_data, dk)
-    g %>% dplyr::glimpse()
-    print(paste("STEP OUTSIDE", st))
-    st <- st + 1
+    # }
   }
-  return(g)
+  return(df)
 }
 
 #' FUNCTION: helper_flip
@@ -206,22 +213,22 @@ helper_bitflip_keep_across <- function(d) {
 
   if ((p + s - 1) > rows) {
     rem <- (p + s - 1) - rows
-      b <- d[1:rem, ]
-      a <- d[(rem + 1):(p - 1), ]
-      m <- d[p:rows, ]
-      bb <- b %>% dplyr::mutate(keep = FALSE) # do the flip
-      u <- m %>% dplyr::mutate(keep = FALSE) # do the flip
-      # print("B")
-      # b %>% dplyr::glimpse()
-      # print("A")
-      # a %>% dplyr::glimpse()
-      # print("M")
-      # m %>% dplyr::glimpse()
-      # print("BB")
-      # bb %>% dplyr::glimpse()
-      # print("U")
-      # u %>% dplyr::glimpse()
-      df <- rbind(bb, a, u)
+    b <- d[1:rem, ]
+    a <- d[(rem + 1):(p - 1), ]
+    m <- d[p:rows, ]
+    bb <- b %>% dplyr::mutate(keep = FALSE) # do the flip
+    u <- m %>% dplyr::mutate(keep = FALSE) # do the flip
+    # print("B")
+    # b %>% dplyr::glimpse()
+    # print("A")
+    # a %>% dplyr::glimpse()
+    # print("M")
+    # m %>% dplyr::glimpse()
+    # print("BB")
+    # bb %>% dplyr::glimpse()
+    # print("U")
+    # u %>% dplyr::glimpse()
+    df <- rbind(bb, a, u)
   } else if ((p + s - 1) == rows) {
     b <- d[1:(p - 1), ]
     m <- d[p:rows, ]

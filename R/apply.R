956f4107ec7f2d5d198fe34c6fedbfc02216fbc5
Colton McCurdy <mccurdyc@allegheny.edu>
2017-04-12 10:28:18 -0400

    created function for applying model to all schemas for a dbms

--- R/apply.R
+++ R/apply.R
@@ -8,3 +8,25 @@ apply_operator_model <- function(d, model) {
   dd <- evaluate_reduction_technique(d, dt)
   return(dd)
 }
+
+#' FUNCTION: apply_operator_model_per_schema
+#'
+#' Apply the model generated from performing hill climbing to each schema.
+#' @export
+
+apply_operator_model_per_schema <- function(d, model) {
+  df <- data.frame()
+  count <- 1
+  schemas <- d %>% select_all_schemas()
+  for (s in schemas[[1]]) {
+    print(paste("current schema ", count, ": ", s))
+    excluded_schema_data <- d %>% select_schema_data(s) # select only the data for the excluded schema
+    for (j in 1:30) {
+      # apply the model informing how many mutants to keep per operator from original data set
+      dt <- excluded_schema_data %>% apply_operator_model(model) %>% transform_add_trial(j) %>% as.data.frame()
+      df <- rbind(df, dt)
+    }
+    count <- count + 1
+  }
+  return(df)
+}
--- R/read.R
+++ R/read.R
@@ -1,13 +1,34 @@
 #' FUNCTION: read_sqlite_avmdefaults
 #'
-#' Read in a data file containing all of the mutation analysis data for sqlite avm defaults. This is
+#' Read in a data file containing all of the mutation analysis data for sqlite avmdefaults. This is
 #' the file containing all of the mutation scores and timings for all schemas
-#' and databases. THIS IS JUST SAMPLE DATA AND A SAMPLE READ FUNCTION.
-#'
+#' and databases.
 #' @export
 
 read_sqlite_avmdefaults <- function(dat) {
   f <- system.file("extdata", "sqlite-avmdefaults.dat", package="mrstudyr")
+  d <- readr::read_csv(f)
+  return(dplyr::tbl_df(d))
+}
+
+#' FUNCTION: read_hypersql_avmdefault
+#'
+#' Read in a data file containing all of the mutation analysis data for the hypersql dbms.
+#' @export
+
+read_hypersql_avmdefaults <- function(dat) {
+  f <- system.file("extdata", "hypersql-avmdefaults.dat", package="mrstudyr")
+  d <- readr::read_csv(f)
+  return(dplyr::tbl_df(d))
+}
+
+#' FUNCTION: read_postgres_avmdefault
+#'
+#' Read in a data file containing all of the mutation analysis data for the postgres dbms.
+#' @export
+
+read_postgres_avmdefaults <- function(dat) {
+  f <- system.file("extdata", "postgres-avmdefaults.dat", package="mrstudyr")
   d <- readr::read_csv(f)
   return(dplyr::tbl_df(d))
 }
